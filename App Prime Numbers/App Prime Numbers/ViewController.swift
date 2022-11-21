7fa1a1657258ce074298cabb06ad1aa1cb7e7e0c
Karen Gabrieli Viana <karengabrieli.viana@outlook.com>
2022-11-21 16:57:28 -0300

    Preliminary structure

---
+++ App Prime Numbers/App Prime Numbers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  App Prime Numbers
+//
+//  Created by Karen Oliveira on 21/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
